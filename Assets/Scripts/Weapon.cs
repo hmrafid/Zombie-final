41ee266a02551fb71e3f488e9a5f97e69672bade
hmrafid <hasan65@student.sust.edu>
2021-12-25 14:12:06 +0600

    Added hit effect

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -9,6 +9,7 @@ public class Weapon : MonoBehaviour
     [SerializeField] float range = 300f;
     [SerializeField] float damage = 30f;
     [SerializeField] ParticleSystem muzzleFlash;
+    [SerializeField] GameObject hitEffect;
 
     void Update()
     {
@@ -34,7 +35,7 @@ public class Weapon : MonoBehaviour
         RaycastHit hit;
         if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
         {
-            Debug.Log("I hit this thing " + hit.transform.name);
+            CreateHitImpact(hit);
             EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
             if (target == null)
                 return;
@@ -44,5 +45,11 @@ public class Weapon : MonoBehaviour
         {
             return;
         }
+    }
+
+    private void CreateHitImpact(RaycastHit hit)
+    {
+        GameObject impact = Instantiate(hitEffect, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(impact, 1);
     }
 }
